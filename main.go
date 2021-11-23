1c5fe1abf77c01bcb013e4c4479a9f44c49bc66b
zrshishir <zrshishir@gmail.com>
2021-11-23 17:13:11 +0600

    working on grpc and protobuf

--- main.go
+++ main.go
@@ -1,13 +1,56 @@
 package main
 
 import (
+	"context"
+	"net"
 	"net/http"
-	"protobuf_grpc/server"
+
+	httpServer "protof_grpc/http"
+	"protof_grpc/loggerfx"
+	pb "protof_grpc/proto"
+	rpcServer "protof_grpc/rpc"
+
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	server.New(mux)
+	fx.New(
+		fx.Provide(http.NewServeMux),
+		fx.Provide(rpcServer.New),
+		fx.Invoke(httpServer.New),
+		fx.Invoke(registerHooks),
+		loggerfx.Module,
+	).Run()
+}
 
-	http.ListenAndServe(":8080", mux)
+func registerHooks(
+	lifecycle fx.Lifecycle, mux *http.ServeMux, logger *zap.SugaredLogger, rpcServer rpcServer.Handler,
+) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+
+				// rpc server
+				lis, err := net.Listen("tcp", ":8081")
+				if err != nil {
+					logger.Fatalf("failed to listen: %v", err)
+				}
+				var opts []grpc.ServerOption
+				grpcServer := grpc.NewServer(opts...)
+				pb.RegisterUsersServer(grpcServer, rpcServer)
+				go grpcServer.Serve(lis)
+
+				// start the http server
+				logger.Info("Listening on localhost:8080 for HTTP requests")
+				go http.ListenAndServe(":8080", mux) // we will look into how to gracefully handle these errors later
+
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				return logger.Sync()
+			},
+		},
+	)
 }
